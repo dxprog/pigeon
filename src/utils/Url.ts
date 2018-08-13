18d18a2d171b299f0f79dc07bb2ef3225838d0c2
Matt Hackmann <matt@dxprog.com>
2018-08-12 23:57:22 -0700

    more query string encoding methods added

--- src/utils/Url.ts
+++ src/utils/Url.ts
@@ -22,13 +22,43 @@ export function cleanUrl(url: string): string {
  * query string with all arguments properly encoded for Rest.li.
  */
 export function encodeRestLiQueryString(queryStringParameters: Dictionary<any>): string {
-  return '';
+  const encodedParameters: Array<string> = Object
+    .keys(queryStringParameters)
+    .map((key: string) => {
+      let value: any = queryStringParameters[key];
+      console.log(key, value);
+      if (Array.isArray(value)) {
+        value = encodeQueryStringList(value);
+      } else if (typeof value === 'object') {
+        value = encodeQueryStringObject(key, value);
+      }
+      return `${key}=${value}`;
+    });
+
+  return encodedParameters.join('&');
 }
 
+/**
+ * Encodes a list of items for a Rest.li request query string
+ * 
+ * @param list The list to encode
+ */
 export function encodeQueryStringList(list: Array<any>): string {
-  return '';
+  return `List(${list.map(encodeURIComponent).join(',')})`;
 }
 
-export function encodeQueryStringObject(obj: Dictionary<any>): string {
-  return '';
+export function encodeQueryStringObject(keyName: string, obj: Dictionary<any>): string {
+  const encodedParameters: Array<string> = Object.keys(obj).map(key => {
+    let value = obj[key];
+    if (Array.isArray(value)) {
+      value = encodeQueryStringList(value);
+    } else if (typeof value === 'object') {
+      value = encodeQueryStringObject(key, value);
+    } else {
+      value = `${key}:${encodeURIComponent(value)}`;
+    }
+    return `(${value})`;
+  });
+
+  return `(${keyName}:${encodedParameters.join(',')})`;
 }
--- tests/utils/Url.spec.ts
+++ tests/utils/Url.spec.ts
@@ -1,21 +1,50 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { cleanUrl } from '../../src/utils/Url';
+import { 
+  cleanUrl,
+  encodeQueryStringList,
+  encodeQueryStringObject,
+  encodeRestLiQueryString } from '../../src/utils/Url';
 
 describe('Url utils', () => {
-  it('should remove extraneous forward slashes in a URL', () => {
-    const cleanedUrl = cleanUrl('this//is-a-very///ugly//url');
-    expect(cleanedUrl).to.equal('this/is-a-very/ugly/url');
+  describe('cleanUrl', () => {
+    it('should remove extraneous forward slashes in a URL', () => {
+      const cleanedUrl = cleanUrl('this//is-a-very///ugly//url');
+      expect(cleanedUrl).to.equal('this/is-a-very/ugly/url');
+    });
+  
+    it('should retain a leading slash on a relative URL', () => {
+      const cleanedUrl = cleanUrl('/back-to-root//please');
+      expect(cleanedUrl).to.equal('/back-to-root/please');
+    });
+  
+    it('should retain leading slashes on a relative scheme URL', () => {
+      const cleanedUrl = cleanUrl('//ssl.ornot.com//please');
+      expect(cleanedUrl).to.equal('//ssl.ornot.com/please');
+    });
   });
 
-  it('should retain a leading slash on a relative URL', () => {
-    const cleanedUrl = cleanUrl('/back-to-root//please');
-    expect(cleanedUrl).to.equal('/back-to-root/please');
+  describe('encodeQueryStringList', () => {
+    it('should properly encode a list', () => {
+      const encodedList = encodeQueryStringList([
+        'Hi there',
+        42,
+        'foobar'
+      ]);
+      expect(encodedList).to.equal('List(Hi%20there,42,foobar)');
+    });
   });
 
-  it('should retain leading slashes on a relative scheme URL', () => {
-    const cleanedUrl = cleanUrl('//ssl.ornot.com//please');
-    expect(cleanedUrl).to.equal('//ssl.ornot.com/please');
+  describe('encodeQueryStringObject', () => {
+    it('should correctly encode a nested object', () => {
+      const encodedObj = encodeRestLiQueryString({
+        searchIntent: {
+          specificScope: 'urn:li:learning:123'
+        }
+      });
+      console.log(encodedObj);
+      expect(true).to.equal(true);
+    });
   });
 });
